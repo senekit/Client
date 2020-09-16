5fb461c1e14cc56c47ffd32e0f36b0e1a2fa32aa
senekit <1792700051@qq.com>
2020-09-16 21:56:41 +0800

    feat：增加了tcp客户端，实现了发送信息功能

--- src/main/java/ClientSocket.java
+++ src/main/java/ClientSocket.java
@@ -1,2 +1,70 @@
-public class ClientSocket {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ClientSocket extends Socket {
+    /**
+     * @Description:无参构造方法
+     * @Author: LJZ
+     * @Date: 2020/9/16 18:20
+    []
+
+     **/
+    ClientSocket(){}
+    /**
+     * @Description:用指定ip和端口号创建客户端套接字对象
+     * @Author: LJZ
+     * @Date: 2020/9/16 18:21
+    [port, ip]
+     **/
+    ClientSocket(String ip,int port) throws IOException {
+        super(ip,port);
+    }
+    /**
+     * @Description:用于发送String类型的消息
+     * @Author: LJZ
+     * @Date: 2020/9/16 18:28
+    [message]
+    void
+     **/
+    public void send(String message) throws IOException {
+        OutputStream os = this.getOutputStream();
+        os.write(message.getBytes());
+        os.flush();
+    }
+    /**
+     * @Description:接收套接字中信息，并进行处理
+     * @Author: LJZ
+     * @Date: 2020/9/16 18:36
+    []
+    java.lang.String
+     **/
+    public void accept() throws IOException {
+        InputStream in = this.getInputStream();
+        byte[] data=new byte[1024];
+        in.read(data);
+        String message =new String(data);
+        System.out.println(message);
+//        String[] messages= new String[100];
+//        int j=0,k=0;
+//        for(int i=0;i<message.length();i++){
+//            if(message.charAt(i)=='/'){
+//                messages[k]=message.substring(j,i);
+//                j=i+1;
+//                k++;
+//            }
+//        }
+//        switch (messages[0].charAt(0)){
+//            case 'S':{
+//                //弹出弹窗提示操作成功
+//            }
+//            case 'I':{
+//                //从服务器获取收入信息
+//            }
+//            case 'E':{
+//                //从服务器获取支出信息
+//            }
+//        }
+    }
 }
