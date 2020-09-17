c85e2524470cd959ac647a0822cc71b22d2ba774
HP <1226023416@qq.com>
2020-09-17 15:14:29 +0800

    feat :新增收入支出界面

--- src/main/java/MainUi.java
+++ src/main/java/MainUi.java
@@ -5,30 +5,34 @@ import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 /**
- * @program: Client
- * @description: 用户主界面
- * @author: yps
- * @create: 2020-09-17 09:38
+ * program: Client
+ * description: 用户主界面
+ * author: yps
+ * create: 2020-09-17 09:38
  **/
 public class MainUi extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         TabPane mainTabPane = new TabPane();
-        Tab myIncomeTab = new Tab("我的收入");           //选项卡界面
-        Tab myExpandTab = new Tab("我的支出");
+        Tab bookingTab = new Tab("记账本");           //选项卡界面
+        Tab myExpandandMyIncomeTab = new Tab("我的收入支出");
         Tab financialAnalysisTab = new Tab("财务分析");
         Tab financialStatementTab = new Tab("财务报表");
         Tab incomeofFamilyMembers = new Tab("家庭成员收支情况");
         Tab financialProjectTab = new Tab("理财项目");
         Tab userInformationTab = new Tab("我的信息");
-        myIncomeTab.setClosable(false);
-        myExpandTab.setClosable(false);
+        bookingTab.setClosable(false);
+        myExpandandMyIncomeTab.setClosable(false);
         financialAnalysisTab.setClosable(false);
         financialStatementTab.setClosable(false);
         incomeofFamilyMembers.setClosable(false);
         financialProjectTab.setClosable(false);
         userInformationTab.setClosable(false);
-        mainTabPane.getTabs().addAll(myExpandTab,myIncomeTab,financialAnalysisTab,financialStatementTab,incomeofFamilyMembers,financialProjectTab,userInformationTab);
+        mainTabPane.getTabs().addAll(bookingTab,myExpandandMyIncomeTab,financialAnalysisTab,financialStatementTab,incomeofFamilyMembers,financialProjectTab,userInformationTab);
+
+        MyExpandAndMyIncomeUi myExpandAndMyIncomeUi = new MyExpandAndMyIncomeUi();
+        myExpandAndMyIncomeUi.init();
+        myExpandandMyIncomeTab.setContent(myExpandAndMyIncomeUi.myExpandandMyIncomePane);
 
         FinancialProjectUi financialProjectUi = new FinancialProjectUi();
         financialProjectUi.init();
---
+++ src/main/java/MyExpandAndMyIncomeUi.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.w3c.dom.CDATASection;
+
+/**
+ * @program: Client
+ * @description: 我的收入支出页面ui
+ * @author: yps
+ * @create: 2020-09-17 11:06
+ **/
+public class MyExpandAndMyIncomeUi {
+    public Pane myExpandandMyIncomePane;
+
+    public void init(){
+        myExpandandMyIncomePane = new Pane();
+
+        DatePicker datePicker = new DatePicker();
+        myExpandandMyIncomePane.getChildren().add(datePicker);
+
+        Button a = new Button("a");
+        myExpandandMyIncomePane.getChildren().add(a);
+
+        a.setOnAction((ActionEvent event) -> {
+            System.out.println(datePicker.getValue());
+        });
+    }
+}
