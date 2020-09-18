fc85b46bcf6aa78b8a16d6d329d6b7bd11349671
HP <1226023416@qq.com>
2020-09-18 11:33:43 +0800

    feat:用户信息UI优化

--- src/main/java/UserInformationUi.java
+++ src/main/java/UserInformationUi.java
@@ -1,4 +1,7 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+
 
 /**
  * @program: Client
@@ -10,6 +13,29 @@ public class UserInformationUi {
     public Pane userInformationPane = new Pane();
 
     public void init(User user){
+        Label backgroundLabel = new Label("                     ");
+        backgroundLabel.setStyle("-fx-background-radius: 10px ;-fx-background-color: gray;");
+        backgroundLabel.setPrefHeight(160);
+        backgroundLabel.setPrefWidth(300);
+        backgroundLabel.setLayoutX(38);
+        backgroundLabel.setLayoutY(40);
 
+        Button changePasswordButton = new Button("修改密码");
+        Button switchAccountButton = new Button("更换账号");
+        Button cancelAccountButton = new Button("注销账户");
+
+
+        changePasswordButton.setLayoutX(150);
+        changePasswordButton.setLayoutY(300);
+        changePasswordButton.setPrefHeight(200);
+        changePasswordButton.setLayoutY(150);
+
+
+
+
+
+
+
+        userInformationPane.getChildren().addAll(backgroundLabel,cancelAccountButton,switchAccountButton,changePasswordButton);
     }
 }
