efd641290cf242ecfe64d4f9d368899cf3dc4d5f
Booker-100 <904677070@qq.com>
2020-09-22 02:07:27 +0800

    feat：还是UI优化 添加了解散家庭提示

--- src/main/java/UserInformationUi.java
+++ src/main/java/UserInformationUi.java
@@ -1,3 +1,4 @@
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -22,16 +23,18 @@ public class UserInformationUi {
 
     public void init(Stage stage){
         Label backgroundLabel = new Label("                     ");
-        backgroundLabel.setStyle("-fx-background-radius: 10px ;-fx-background-color: gray;");
-        backgroundLabel.setPrefHeight(160);
-        backgroundLabel.setPrefWidth(300);
-        backgroundLabel.setLayoutX(66);
+        backgroundLabel.setStyle("-fx-background-radius: 10px ;-fx-background-color: #acadaf;");
+        backgroundLabel.setPrefHeight(200);
+        backgroundLabel.setPrefWidth(380);
+        backgroundLabel.setLayoutX(26);
         backgroundLabel.setLayoutY(20);
 
-        Image userImage = new Image("file:///D:/Study/Client/src/main/Image/timg.png");
+        Image userImage = new Image("file:D:/SoftwareEngineering/2020(2)/Java/Demo/jdbc/Client/src/main/Image/timg.png");
         ImageView userImageView = new ImageView(userImage);
-        userImageView.setFitHeight(100);
-        userImageView.setFitWidth(100);
+        userImageView.setFitHeight(120);
+        userImageView.setFitWidth(120);
+        userImageView.setX(40);
+        userImageView.setY(40);
 
         Button changePasswordButton = new Button("修改密码");
         Button switchAccountButton = new Button("更换账号");
@@ -40,27 +43,27 @@ public class UserInformationUi {
         Button cancelButton = new Button("退出程序");
 
         deleteButton.setLayoutX(260);
-        deleteButton.setLayoutY(140);
+        deleteButton.setLayoutY(170);
         deleteButton.setPrefHeight(30);
-        deleteButton.setPrefWidth(80);
+        deleteButton.setPrefWidth(110);
 
-        changePasswordButton.setLayoutX(138);
-        changePasswordButton.setLayoutY(205);
-        changePasswordButton.setPrefHeight(50);
-        changePasswordButton.setPrefWidth(150);
+        changePasswordButton.setLayoutX(40);
+        changePasswordButton.setLayoutY(170);
+        changePasswordButton.setPrefHeight(30);
+        changePasswordButton.setPrefWidth(110);
 
         switchAccountButton.setLayoutX(138);
-        switchAccountButton.setLayoutY(270);
+        switchAccountButton.setLayoutY(260);
         switchAccountButton.setPrefHeight(50);
         switchAccountButton.setPrefWidth(150);
 
         cancelAccountButton.setLayoutX(138);
-        cancelAccountButton.setLayoutY(335);
+        cancelAccountButton.setLayoutY(340);
         cancelAccountButton.setPrefHeight(50);
         cancelAccountButton.setPrefWidth(150);
 
         cancelButton.setLayoutX(138);
-        cancelButton.setLayoutY(400);
+        cancelButton.setLayoutY(420);
         cancelButton.setPrefHeight(50);
         cancelButton.setPrefWidth(150);
 
@@ -71,7 +74,7 @@ public class UserInformationUi {
                         "-fx-font-size:16px;");
         deleteButton.setOnMouseEntered(
                 e->{
-                    deleteButton.setStyle("-fx-background-color: #e2e3e7;\n" +
+                    deleteButton.setStyle("-fx-background-color: red;\n" +
                             "-fx-background-radius: 5;\n" +
                             "-fx-border-radius: 5;"+
                             "-fx-font-size:16px;");
@@ -85,7 +88,7 @@ public class UserInformationUi {
                 });
         deleteButton.setOnMousePressed(
                 e->{
-                    deleteButton.setStyle("-fx-background-color: #b5b6ba;\n" +
+                    deleteButton.setStyle("-fx-background-color: darkred;\n" +
                             "-fx-background-radius: 5;\n" +
                             "-fx-border-radius: 5;"+
                             "-fx-font-size:16px;");
@@ -98,6 +101,7 @@ public class UserInformationUi {
                                     "-fx-border-radius: 5;"+
                                     "-fx-font-size:16px;");
                 });
+
 
         changePasswordButton.setStyle(
                 "-fx-background-color: #f9fbff;\n" +
@@ -246,17 +250,17 @@ public class UserInformationUi {
         Text nameText = new Text("姓名：" + MainUi.user.getName());
         Text familyIdText = new Text("家庭ID：" + MainUi.user.getFamilyId());
 
-        emailText.setLayoutX(85);
-        emailText.setLayoutY(55);
-        emailText.setStyle("-fx-font-size: 30px;");
+        emailText.setLayoutX(170);
+        emailText.setLayoutY(60);
+        emailText.setStyle("-fx-font-size: 18px;");
 
-        nameText.setLayoutX(85);
-        nameText.setLayoutY(110);
-        nameText.setStyle("-fx-font-size: 30px;");
+        nameText.setLayoutX(170);
+        nameText.setLayoutY(107);
+        nameText.setStyle("-fx-font-size: 18px;");
 
-        familyIdText.setLayoutX(85);
-        familyIdText.setLayoutY(165);
-        familyIdText.setStyle("-fx-font-size: 30px;");
+        familyIdText.setLayoutX(170);
+        familyIdText.setLayoutY(155);
+        familyIdText.setStyle("-fx-font-size: 18px;");
 
         userInformationPane.getChildren().addAll(emailText,nameText,familyIdText);
 
@@ -285,14 +289,20 @@ public class UserInformationUi {
         });
 
         //解散
-        deleteButton.setOnAction(e->{
-            try {
-                ClientSocket socket = new ClientSocket("127.0.0.1",8888);
-                socket.send(new String("Z/"+MainUi.user.getFamilyId()));
-                familyIdText.setText("家庭ID：0");
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        });
+        deleteButton.setOnMouseClicked(
+                e-> {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                    alert.setTitle("注意");
+                    alert.setHeaderText(null);
+                    alert.setContentText("确定要解散家庭吗（该操作不可撤销）");
+                    alert.showAndWait();
+                        try {
+                            ClientSocket socket = new ClientSocket("127.0.0.1", 8888);
+                            socket.send(new String("Z/" + MainUi.user.getFamilyId()));
+                            familyIdText.setText("家庭ID：0");
+                        } catch (IOException ioException) {
+                            ioException.printStackTrace();
+                        }
+                    });
     }
 }
